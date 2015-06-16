c2b8af63648b1ff7f766efdb8f17c094892f7110
Andrea Mazzini <andrea.mazzini@gmail.com>
2015-06-16 08:43:44 +0200

    Add Carthage support

---
+++ AMViralSwitch/AMViralSwitch.h
@@ -0,0 +1,19 @@
+//
+//  AMViralSwitch.h
+//  AMViralSwitch
+//
+//  Created by Andrea Mazzini on 16/06/15.
+//  Copyright (c) 2015 Fancy Pixel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AMViralSwitch.
+FOUNDATION_EXPORT double AMViralSwitchVersionNumber;
+
+//! Project version string for AMViralSwitch.
+FOUNDATION_EXPORT const unsigned char AMViralSwitchVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AMViralSwitch/PublicHeader.h>
+#import <AMViralSwitch/AMViralSwitch.h>
+
