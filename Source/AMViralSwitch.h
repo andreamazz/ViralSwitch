dacecfee20376b2495997f36db31367ae655bad8
Andrea Mazzini <andrea.mazzini@gmail.com>
2015-11-01 15:33:24 +0100

    Update demo projct

--- Source/AMViralSwitch.h
+++ Source/AMViralSwitch.h
@@ -6,7 +6,7 @@
 //  Copyright (c) 2014 Fancy Pixel. All rights reserved.
 //
 
-@import UIKit;
+#import <UIKit/UIKit.h>
 
 /** @constant AMViralSwitch AMElementView View animated alongside the switch */
 FOUNDATION_EXPORT NSString *const AMElementView;
